ebe4d435ad04ac86f3067940bdf1161de2d3a01e
toxic14 <75319001+toxic14@users.noreply.github.com>
2020-12-14 17:06:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var car,wall;
+
+var speed,weight;
+
+function setup() {
+  createCanvas(800,400);
+
+  speed=random(55,90);
+  weight=random(400,1500);
+
+  wall=createSprite(700, 200, 60, height/2);
+
+  car=createSprite(100, 200, 20, 20);
+  car.velocityX=speed;
+  car.shapeColor="white";
+
+}
+
+function draw() {
+  background(0);  
+
+  if(wall.x - car.x < car.width/2 + wall.width/2) {
+        
+        car.velocityX=0;
+
+     var deformation=0.5*weight*speed*speed/22509;
+
+     if(deformation>180){
+
+      car.shapeColor=color(255,0,0);
+
+}
+
+if(deformation<100){
+
+  car.shapeColor=color(0,255,0);
+
+}
+
+   if(deformation<180 && deformation>100){
+
+     car.shapeColor=color(230,230,0);
+
+}
+
+  }
+
+  drawSprites();
+}
